ca449a58fccf423a840274baa4e2118d5a633ac1
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:41:39 +0200

    add javadoc for UsernameCategoryDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/UsernameCategoryDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/UsernameCategoryDTO.java
@@ -16,26 +16,50 @@ public class UsernameCategoryDTO implements Serializable {
      */
     private String category;
 
+    /**
+     * Constructor
+     */
     public UsernameCategoryDTO() {
     }
 
+    /**
+     * Constructor
+     * @param username username of the user
+     * @param category category of the quiz
+     */
     public UsernameCategoryDTO(String username, String category) {
         this.username = username;
         this.category = category;
     }
 
+    /**
+     * Getter for username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     * @param username username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for category
+     * @return category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Setter for category
+     * @param category category
+     */
     public void setCategory(String category) {
         this.category = category;
     }

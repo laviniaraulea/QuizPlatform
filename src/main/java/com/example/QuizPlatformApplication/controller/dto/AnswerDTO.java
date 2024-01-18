c318a8a1f9f8b27095a0827ff7a9f6eff18f8e93
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:17:18 +0200

    add javadoc for AnswerDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/AnswerDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/AnswerDTO.java
@@ -16,26 +16,50 @@ public class AnswerDTO implements Serializable {
      */
     private List<String> answersIds;
 
+    /**
+     * Constructor
+     * @param questionId question id as string
+     * @param answersIds list of answers ids as strings
+     */
     public AnswerDTO(String questionId, List<String> answersIds) {
         this.questionId = questionId;
         this.answersIds = answersIds;
     }
 
+    /**
+     * Constructor
+     */
     public AnswerDTO() {
     }
 
+    /**
+     * Getter for question id
+     * @return question id as string
+     */
     public String getQuestionId() {
         return questionId;
     }
 
+    /**
+     * Setter for question id
+     * @param questionId question id as string
+     */
     public void setQuestionId(String questionId) {
         this.questionId = questionId;
     }
 
+    /**
+     * Getter for answers ids
+     * @return list of answers ids as strings
+     */
     public List<String> getAnswersIds() {
         return answersIds;
     }
 
+    /**
+     * Setter for answers ids
+     * @param answersIds list of answers ids as strings
+     */
     public void setAnswersIds(List<String> answersIds) {
         this.answersIds = answersIds;
     }

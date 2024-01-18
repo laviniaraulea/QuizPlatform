34326c4d6b70daca1e1ddc182ad0d5fe1eea9fb1
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:40:42 +0200

    add javadoc for UserDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/UserDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/UserDTO.java
@@ -17,35 +17,68 @@ public class UserDTO {
      */
     private String dateOfBirth;
 
+    /**
+     * Constructor
+     */
     public UserDTO() {
     }
 
+    /**
+     * Constructor
+     * @param username username of the user
+     * @param password password of the user
+     * @param dateOfBirth date of birth of the user
+     */
     public UserDTO(String username, String password, String dateOfBirth) {
         this.username = username;
         this.password = password;
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * Getter for username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     * @param username username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for password
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     * @param password password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Getter for date of birth
+     * @return date of birth
+     */
     public String getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     * Setter for date of birth
+     * @param dateOfBirth date of birth
+     */
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }

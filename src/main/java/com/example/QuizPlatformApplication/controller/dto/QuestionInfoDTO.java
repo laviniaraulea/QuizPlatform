07f388f9cca0d990cad6a8f60970e30b17fdcab4
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:34:40 +0200

    add javadoc for QuestionInfoDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/QuestionInfoDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/QuestionInfoDTO.java
@@ -26,6 +26,13 @@ public class QuestionInfoDTO implements Serializable {
      */
     private List<Pair<Long, String>> possibleAnswers;
 
+    /**
+     * Constructor
+     * @param questionId question id
+     * @param isMultipleChoice boolean value indicating if the question is multiple choice
+     * @param questionText question text
+     * @param possibleAnswers list of pairs of option ids and question texts
+     */
     public QuestionInfoDTO(Long questionId, boolean isMultipleChoice, String questionText, List<Pair<Long, String>> possibleAnswers) {
         this.questionId = questionId;
         this.isMultipleChoice = isMultipleChoice;
@@ -33,37 +40,72 @@ public class QuestionInfoDTO implements Serializable {
         this.possibleAnswers = possibleAnswers;
     }
 
+    /**
+     * Constructor
+     */
     public QuestionInfoDTO() {
     }
 
+    /**
+     * Getter for question id
+     * @return question id
+     */
     public Long getQuestionId() {
         return questionId;
     }
 
+    /**
+     * Setter for question id
+     * @param questionId question id
+     */
     public void setQuestionId(Long questionId) {
         this.questionId = questionId;
     }
 
+    /**
+     * Getter for boolean value indicating if the question is multiple choice
+     * @return boolean value indicating if the question is multiple choice
+     */
     public boolean isMultipleChoice() {
         return isMultipleChoice;
     }
 
+    /**
+     * Setter for boolean value indicating if the question is multiple choice
+     * @param multipleChoice boolean value indicating if the question is multiple choice
+     */
     public void setMultipleChoice(boolean multipleChoice) {
         isMultipleChoice = multipleChoice;
     }
 
+    /**
+     * Getter for question text
+     * @return question text
+     */
     public String getQuestionText() {
         return questionText;
     }
 
+    /**
+     * Setter for question text
+     * @param questionText question text
+     */
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
     }
 
+    /**
+     * Getter for list of pairs of option ids and question texts
+     * @return list of pairs of option ids and question texts
+     */
     public List<Pair<Long, String>> getPossibleAnswers() {
         return possibleAnswers;
     }
 
+    /**
+     * Setter for list of pairs of option ids and question texts
+     * @param possibleAnswers list of pairs of option ids and question texts
+     */
     public void setPossibleAnswers(List<Pair<Long, String>> possibleAnswers) {
         this.possibleAnswers = possibleAnswers;
     }

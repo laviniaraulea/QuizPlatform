0366a9a631622e075ab1085e24995ae3d4178885
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:32:23 +0200

    add javadoc for GeneralStatsDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/GeneralStatsDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/GeneralStatsDTO.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  */
 public class GeneralStatsDTO implements Serializable {
     /**
-     * number of total quizzes creaded by the user as long
+     * number of total quizzes created by the user as long
      */
     private long totalQuizzesCreated;
 
@@ -21,34 +21,67 @@ public class GeneralStatsDTO implements Serializable {
      */
     private float averageScoreQuizzes;
 
+    /**
+     * Constructor
+     * @param totalQuizzesCreated number of total quizzes created by the user
+     * @param totalQuizzesDone number of total quizzes done by the user
+     * @param averageScoreAllQuizzes average score of a user
+     */
     public GeneralStatsDTO(long totalQuizzesCreated, long totalQuizzesDone, float averageScoreAllQuizzes) {
         this.totalQuizzesCreated = totalQuizzesCreated;
         this.totalQuizzesDone = totalQuizzesDone;
         this.averageScoreQuizzes = averageScoreAllQuizzes;
     }
 
+    /**
+     * Constructor
+     */
     public GeneralStatsDTO() {}
 
+    /**
+     * Getter for number of total quizzes created by the user
+     * @return number of total quizzes created by the user
+     */
     public long getTotalQuizzesCreated() {
         return totalQuizzesCreated;
     }
 
+    /**
+     * Setter for number of total quizzes created by the user
+     * @param totalQuizzesCreated number of total quizzes created by the user
+     */
     public void setTotalQuizzesCreated(long totalQuizzesCreated) {
         this.totalQuizzesCreated = totalQuizzesCreated;
     }
 
+    /**
+     * Getter for number of total quizzes done by the user
+     * @return number of total quizzes done by the user
+     */
     public long getTotalQuizzesDone() {
         return totalQuizzesDone;
     }
 
+    /**
+     * Setter for number of total quizzes done by the user
+     * @param totalQuizzesDone number of total quizzes done by the user
+     */
     public void setTotalQuizzesDone(long totalQuizzesDone) {
         this.totalQuizzesDone = totalQuizzesDone;
     }
 
+    /**
+     * Getter for average score of a user
+     * @return average score of a user
+     */
     public float getAverageScoreAllQuizzes() {
         return averageScoreQuizzes;
     }
 
+    /**
+     * Setter for average score of a user
+     * @param averageScoreAllQuizzes average score of a user
+     */
     public void setAverageScoreAllQuizzes(float averageScoreAllQuizzes) {
         this.averageScoreQuizzes = averageScoreAllQuizzes;
     }

0d42957f3ff577adb06b9898ff06c24ee01746f3
phxro <72880964+phxro@users.noreply.github.com>
2024-01-18 21:27:13 +0200

    add javadoc for FailedAndPassDTO

--- src/main/java/com/example/QuizPlatformApplication/controller/dto/FailedAndPassDTO.java
+++ src/main/java/com/example/QuizPlatformApplication/controller/dto/FailedAndPassDTO.java
@@ -16,25 +16,45 @@ public class FailedAndPassDTO implements Serializable {
      */
     private long noPassQuizzes;
 
+    /**
+     * Constructor
+     * @param noFailedQuizzes number of failed quizzes
+     * @param noPassQuizzes number of passed quizzes
+     */
     public FailedAndPassDTO(long noFailedQuizzes, long noPassQuizzes) {
         this.noFailedQuizzes = noFailedQuizzes;
         this.noPassQuizzes = noPassQuizzes;
     }
 
+    /**
+     * Getter for number of failed quizzes
+     * @return number of failed quizzes
+     */
     public long getNoFailedQuizzes() {
         return noFailedQuizzes;
     }
 
+    /**
+     * Setter for number of failed quizzes
+     * @param noFailedQuizzes number of failed quizzes
+     */
     public void setNoFailedQuizzes(long noFailedQuizzes) {
         this.noFailedQuizzes = noFailedQuizzes;
     }
 
+    /**
+     * Getter for number of passed quizzes
+     * @return number of passed quizzes
+     */
     public long getNoPassQuizzes() {
         return noPassQuizzes;
     }
 
+    /**
+     * Setter for number of passed quizzes
+     * @param noPassQuizzes number of passed quizzes
+     */
     public void setNoPassQuizzes(long noPassQuizzes) {
         this.noPassQuizzes = noPassQuizzes;
     }
 }
-
